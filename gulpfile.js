41e6480cc2eac85a5077451137819bc324f1d670
Alexander_V <moki1444@gmail.com>
2021-08-10 00:13:27 +0300

    reconstruction folders

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+let project_folder = 'dist';
+let source_folder = 'app';
+let path = {
+  build: {
+    html: project_folder + '/',
+    css: project_folder + '/css/',
+    js: project_folder + '/js/',
+    img: project_folder + '/img/',
+    fonts: project_folder + '/fonts/',
+  },
+  src: {
+    html: source_folder + '/*.html',
+    css: source_folder + '/scss/style.scss',
+    js: source_folder + '/js/script.js',
+    img: source_folder + '/img/**/*.{jpg, png, svg, gif, ico, webp}',
+    fonts: source_folder + '/fonts/*.ttf',
+  },
+  watch: {
+    html: source_folder + '/**/*.html',
+    css: source_folder + '/scss/**/*.scss',
+    js: source_folder + '/js/**/*.js',
+    img: source_folder + '/img/**/*.{jpg, png, svg, gif, ico, webp}',
+  },
+  clean: './' + project_folder + '/',
+};
+
+let { src, dest } = require('gulp'),
+  gulp = require('gulp');
+
+function html() {
+  return src(path.src.html).pipe(dest(path.build.html)).pipe();
+}
