39a1ca4700748bc5468cea8519f552cfe48c378e
Alexander_V <moki1444@gmail.com>
2021-08-17 12:57:43 +0300

    add alert on buttons

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,2 +1,10 @@
-import { sum } from './helpers';
+import { sum } from './button';
 console.log(sum(3, 7));
+
+const buttons = document.querySelectorAll('.button');
+for (let i = 0; i < buttons.length; i++) {
+    const button = buttons[i];
+    button.addEventListener('click', () => {
+        alert('Карточка добавлена в корзину');
+    });
+}
