53827be95802ebbe65dc6c6bada39983cf1a77cf
Alexander_V <moki1444@gmail.com>
2021-08-19 17:48:52 +0300

    decompose slider components

--- src/ts/slider.ts
+++ src/ts/slider.ts
@@ -1,39 +1,31 @@
-const leftButton = document.querySelector('.slider__button--left');
-const rightButton = document.querySelector('.slider__button--right');
+const leftButton = <HTMLButtonElement>(
+    document.querySelector('.slider__button--left')
+);
+const rightButton = <HTMLButtonElement>(
+    document.querySelector('.slider__button--right')
+);
 
 const sliderScroller = <HTMLElement>document.querySelector('.slider-scroller');
+const slidesCount = document.querySelectorAll('.slider-item').length;
 let currentSlide: number = 0;
 
 rightButton.addEventListener('click', slideRight);
 leftButton.addEventListener('click', slideLeft);
 
+function slideShow() {
+    sliderScroller.style.transform = `translateX(-${25 * currentSlide}%)`;
+    leftButton.disabled = !currentSlide;
+    rightButton.disabled = currentSlide + 4 === slidesCount;
+}
+
 function slideRight() {
     currentSlide++;
-
-    switch (currentSlide) {
-        case 1:
-            leftButton.removeAttribute('disabled');
-            break;
-
-        case 3:
-            rightButton.setAttribute('disabled', 'disabled');
-            break;
-    }
-
-    sliderScroller.style.transform = `translateX(-${25 * currentSlide}%)`;
+    slideShow();
 }
 
 function slideLeft() {
     currentSlide--;
-
-    switch (currentSlide) {
-        case 0:
-            leftButton.setAttribute('disabled', 'disabled');
-            break;
-        case 2:
-            rightButton.removeAttribute('disabled');
-            break;
-    }
-
-    sliderScroller.style.transform = `translateX(-${25 * currentSlide}%)`;
+    slideShow();
 }
+
+slideShow();
